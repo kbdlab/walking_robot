c1d52b8c0fd9921ef43aa1ee951f0766d5bc2676
HwangSehyun <hwanghyun3@gmail.com>
2020-11-04 03:43:41 +0900

    init.sh

--- init/init.sh
+++ init/init.sh
@@ -1,9 +1,6 @@
 set -e
-sudo apt update
-sudo apt install libqt5gui5
 sudo raspi-config
 wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
-sudo dpkg -i teamviewer-host_armhf.deb
-sudo apt --fix-broken install 
+sudo apt install ./teamviewer-host_armhf.deb 
 sudo teamviewer setup
 sudo teamviewr info
