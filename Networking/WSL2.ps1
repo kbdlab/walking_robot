b455e30eeb1ca5fb780a79cfd4563ebdbd38cd9e
HwangSehyun <hwanghyun3@gmail.com>
2020-11-10 00:06:28 +0900

    Added port-fowarding to WSL2

--- Networking/WSL2.ps1
+++ Networking/WSL2.ps1
@@ -1,13 +1,41 @@
 # Usage: In an admin powershell,
 # Set-ExecutionPolicy RemoteSigned
-# .\WLS2.ps1 <PORT>
+# .\WLS2.ps1
 
-$port=$args[0]
-echo $port
+$remoteport = wsl bash -c " ip addr show dev eth0 | grep 'inet ' | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | head -1"
+$found = $remoteport -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
 
-$wsl_host = wsl bash -c " ip addr show dev eth0 | grep 'inet ' | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | head -1"
-echo $wsl_host
+if( $found ){
+  $remoteport = $matches[0];
+} else{
+  echo "The Script Exited, the ip address of WSL 2 cannot be found";
+  exit;
+}
 
-netsh interface portproxy add v4tov4 listenport=$port listenaddress=0.0.0.0 connectport=$port connectaddress=$host
+#[Ports]
+
+#All the ports you want to forward separated by coma
+$ports=@(8000);
+
+
+#[Static ip]
+#You can change the addr to your ip config to listen to a specific address
+$addr='0.0.0.0';
+$ports_a = $ports -join ",";
+
+
+#Remove Firewall Exception Rules
+iex "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' ";
+
+#adding Exception Rules for inbound and outbound Rules
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+
+for( $i = 0; $i -lt $ports.length; $i++ ){
+  $port = $ports[$i];
+  iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
+  iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
+}
+
 
 echo Success
