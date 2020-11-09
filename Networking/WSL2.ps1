e40f6f6a108f1b945a2550b839d7bccbc754633d
HwangSehyun <hwanghyun3@gmail.com>
2020-11-10 00:01:13 +0900

    Added port-fowarding to WSL2

--- Networking/WSL2.ps1
+++ Networking/WSL2.ps1
@@ -5,8 +5,8 @@
 $port=$args[0]
 echo $port
 
-$host = wsl bash -c " ip addr show dev eth0 | grep 'inet ' | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | head -1"
-echo $host
+$wsl_host = wsl bash -c " ip addr show dev eth0 | grep 'inet ' | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | head -1"
+echo $wsl_host
 
 netsh interface portproxy add v4tov4 listenport=$port listenaddress=0.0.0.0 connectport=$port connectaddress=$host
 
