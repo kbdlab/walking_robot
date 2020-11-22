6e3daa2e8298e6df7bbf3782b0eec380d79503c3
aegggsehyun1994 <hwanghyun3@gmail.com>
2020-11-22 18:20:52 +0900

    Reverted to 2019

--- init/init.sh
+++
@@ -1,6 +0,0 @@
-set -e
-sudo raspi-config
-wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
-sudo apt install ./teamviewer-host_armhf.deb 
-sudo teamviewer setup
-sudo teamviewr info
